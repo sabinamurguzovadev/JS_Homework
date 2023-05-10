15ec9e46674f8514718945682fe648af674fecdd
Sabina Murguzova <120595196+sabinamurguzovadev@users.noreply.github.com>
2023-05-10 08:59:25 +0400

    Add files via upload

---
+++ 8.05 Await/script.js
@@ -0,0 +1,42 @@
+
+// let row = document.querySelector(".row");
+// function getDataById(id) {
+//   fetch(`https://northwind.vercel.app/api/customers/${id}`)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// }
+
+
+// // getDataById();
+
+// function drowcards(items) {
+//   items.forEach((element) => {
+//     row.innerHTML = "";
+//     row.innerHTML += `
+//     span class="col col-6  ">
+//         <div class="card " style="width: 30rem">
+//           <div class="card-body d-flex  ">
+//             <div>
+//               <h5 class="card-title">${element.name}</h5>
+//               <p class="card-text">
+//                 ${element.email}
+//               </p>
+//             </div>
+//             <div class="d-flex gap-3 "><i class="fa-solid fa-pen"></i>
+//               <i class="fa-solid fa-trash"></i>
+//             </div>
+//           </div>
+//         </div>
+//       </span>
+//      `;
+//   });
+// }
+
+// drowcards();
+
+
+
+// function allData(){
+//   // fetch("https://northwind.vercel.app/api/customers").then((res)=>res.json()).then((data)=>console.log(data))
+// }
